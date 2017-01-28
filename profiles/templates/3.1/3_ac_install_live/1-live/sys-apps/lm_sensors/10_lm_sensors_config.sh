c40ae33de275501675fab0d89c9a62446cb0724e
mavriq <admin@mavriq.net>
2017-01-28 20:59:43 +0300

    currently profiles and sets in this overlay is not necessary

--- profiles/templates/3.1/3_ac_install_live/1-live/sys-apps/lm_sensors/10_lm_sensors_config.sh
+++
@@ -1,5 +0,0 @@
-# Calculate exec=/bin/bash exists(/etc/conf.d/lm_sensors)==
-
-echo "Run sensors-detect in background"
-sh -c "( /bin/yes '' | /usr/sbin/sensors-detect; /usr/bin/touch /etc/conf.d/lm_sensors; /etc/init.d/lm_sensors start ) &>/dev/null &"
-:
--- profiles/templates/3.1/6_ac_desktop_profile/2-user/app-crypt/seahorse-3/.dconfigure.d/seahorse_by_keyring.sh
+++
@@ -1,4 +0,0 @@
-# Calculate path=~/.dconfigure.d chmod=700
-#!/bin/sh
-
-dconf write /apps/seahorse/listing/sidebar-visible true
